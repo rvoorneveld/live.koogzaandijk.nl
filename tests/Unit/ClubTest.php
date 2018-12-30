3e9e3cbdfcb94fa45c24fbe681f99b64e81272e3
Rick Voorneveld <r.voorneveld@hoteliers.com>
2018-12-30 14:11:03 +0100

    Added test for hasMany Relationship

--- tests/Unit/ClubTest.php
+++ tests/Unit/ClubTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Club;
+use App\Team;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -17,6 +18,14 @@ class ClubTest extends TestCase
         $club = factory(Club::class)->create();
 
         $this->assertEquals("/club/{$club->id}", $club->path());
+    }
+
+    public function testItHasTeams(): void
+    {
+        $club = factory(Club::class)->create();
+        factory(Team::class, $expected = 2)->create(['club_id' => $club->id]);
+
+        $this->assertCount($expected, Club::find($club->id)->teams()->get());
     }
 
 }
